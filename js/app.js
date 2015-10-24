1bd77cb8e113c03ed49f60a65fd8fb4b2c36faee
Jonas Kleiner <kleinerdesign@gmail.com>
2015-10-24 15:15:17 -0700

    fizzbuzz with user input works

--- js/app.js
+++ js/app.js
@@ -10,25 +10,22 @@ $(document).ready(function() {
 		console.log("parsed num " + n); // display parsed value in console
 		$("#userChoice").append("user number is " + n); // display parsed value on page
 		$("#fizzCount").val('');
+
+		 // fizz buzz starts here
+
+			for ( var i = 1; i <= n; i++ ) {
+				if (i % 3 === 0 && i % 5 === 0 ) {
+					$("#theValues").append("<p>fizzbuzz</p>");
+				} else if (i % 3 === 0) {
+					$("#theValues").append("<p>fizz</p>");
+				} else if (i % 5 === 0) {
+					$("#theValues").append("<p>buzz</p>");
+				} else {
+					$("#theValues").append("<p>"+i+"</p>");
+				}
+			}
+
 	}
 	
 	$("#getCountNumber").on('submit', processInput);
-
-	function fizzbuzztime() {
-
-		// fizz buzz starts here
-
-		for ( var i = 1; i <= n; i++ ) {
-		
-			if (i % 3 === 0 && i % 5 === 0 ) {
-				$("#theValues").append("<p>fizzbuzz</p>");
-			} else if (i % 3 === 0) {
-				$("#theValues").append("<p>fizz</p>");
-			} else if (i % 5 === 0) {
-				$("#theValues").append("<p>buzz</p>");
-			} else {
-				$("#theValues").append("<p>"+i+"</p>");
-			}
-		}
-	}
 });
