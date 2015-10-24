f9699a068a86c18e7b59a7c718233f926dc72b94
Jonas Kleiner <kleinerdesign@gmail.com>
2015-10-24 15:07:10 -0700

    converted user string dislplays on screen

--- js/app.js
+++ js/app.js
@@ -1,33 +1,34 @@
 $(document).ready(function() {
-	// button submit (id formVal) pushes an input string 
-	// grab input string from form id "fizzCount"
-	// check if input string is a number
-	// convert input string to an integer
-	// assign the integer to a variable called "the_count"
-	// print the value of "the_count" in console.log
+
+	var n = 0;
 
 	function processInput() {
 		event.preventDefault(); // prevents form from refreshing page
 		var process_string = $("#fizzCount").val();  // raw form value
 		console.log(process_string); // display raw form value
-		var converted_string = parseInt(process_string); // convert string to integer
-		console.log(converted_string); // display converted value
+		var n = parseInt(process_string); // convert string to integer
+		console.log("parsed num " + n); // display parsed value in console
+		$("#userChoice").append("user number is " + n); // display parsed value on page
+		$("#fizzCount").val('');
 	}
 	
 	$("#getCountNumber").on('submit', processInput);
 
+	function fizzbuzztime() {
+
 		// fizz buzz starts here
 
-		/*for ( var i = 1; i <= converted_string; i++ ) {
+		for ( var i = 1; i <= n; i++ ) {
 		
-		if (i % 3 === 0 && i % 5 === 0 ) {
-			$("#theValues").append("<p>fizzbuzz</p>");
-		} else if (i % 3 === 0) {
-			$("#theValues").append("<p>fizz</p>");
-		} else if (i % 5 === 0) {
-			$("#theValues").append("<p>buzz</p>");
-		} else {
-			$("#theValues").append("<p>"+i+"</p>");
+			if (i % 3 === 0 && i % 5 === 0 ) {
+				$("#theValues").append("<p>fizzbuzz</p>");
+			} else if (i % 3 === 0) {
+				$("#theValues").append("<p>fizz</p>");
+			} else if (i % 5 === 0) {
+				$("#theValues").append("<p>buzz</p>");
+			} else {
+				$("#theValues").append("<p>"+i+"</p>");
+			}
 		}
-	}*/
+	}
 });
